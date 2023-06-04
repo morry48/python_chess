206912b36d4c4cbb531727cc7138baf9e42aa7b4
morry48 <yyyyuuuuuuuu0408@gmail.com>
2023-06-04 14:40:02 +0900

    7.12 Task: Add your first tests with already set func

--- test_chess_puzzle.py
+++ test_chess_puzzle.py
@@ -6,8 +6,36 @@ def test_location2index1():
     assert location2index("e2") == (5, 2)
 
 
+def test_location2indexCaseMax():
+    assert location2index("z26") == (26, 26)
+
+
+def test_location2indexCaseMin():
+    assert location2index("a1") == (1, 1)
+
+    # todo case for location2index
+    # 0
+    # 27
+    # not start alfabet
+    # not start alfabet
+    # including not int
+    # whether is capital permitted
+
+
 def test_index2location1():
     assert index2location(5, 2) == "e2"
+
+
+def test_index2locationMax():
+    assert index2location(26, 26) == "z26"
+
+
+def test_index2locationMin():
+    assert index2location(1, 1) == "a1"
+
+    # todo case for index2location
+    # 0
+    # 27
 
 
 wb1 = Bishop(2, 5, True)
@@ -32,16 +60,107 @@ def test_is_piece_at1():
     assert is_piece_at(2, 2, B1) == False
 
 
+def test_is_piece_at_case_true():
+    assert is_piece_at(2, 5, B1) == True
+
+    # todo case for is_piece_at
+    # black
+    # white
+    # King
+    #
+    # out of range
+    # than 27
+    # less 0
+    # assert is_piece_at(2, 5, B1) == True
+
+
 def test_piece_at1():
     assert piece_at(3, 3, B1) == bb1
+
+
+def test_piece_at_for_false_side():
+    assert piece_at(3, 3, B1) != wb1
+
+
+def test_piece_at_for_true_side():
+    assert piece_at(5, 3, B1) == bb2
+
+    # todo case for piece_at
+    # not found
 
 
 def test_can_reach1():
     assert wb2.can_reach(5, 5, B1) == True
 
 
+def test_can_reach_for_diagonal_ok_but_the_same_side():
+    assert wb2.can_reach(3, 5, B1) == False
+
+
+def test_can_reach_for_diagonal_ok_and_the_opponent_side():
+    assert wb2.can_reach(5, 3, B1) == True
+    assert bb1.can_reach(4, 4, B1) == True
+
+
+def test_can_reach_for_the_opponent_side_but_there_are_opponent_piece_between_them():
+    assert bb1.can_reach(5, 5, B1) == False
+
+
+def test_can_reach_for_firstly_same():
+    assert wb2.can_reach(4, 4, B1) == False
+
+
+def test_can_reach_for_over_range_for_borad():
+    assert wb2.can_reach(6, 6, B1) == False
+    assert wb1.can_reach(1, 6, B1) == False
+
+
+def test_can_reach_for_king_vertical():
+    assert wk1.can_reach(3, 4, B1) == True
+    assert wk1.can_reach(4, 5, B1) == True
+    assert wk1.can_reach(4, 6, B1) == True
+
+
+def test_can_reach_for_more_than_two():
+    assert wk1.can_reach(3, 7, B1) == False
+
+
+def test_can_reach_for_the_same_side():
+    assert wk1.can_reach(4, 4, B1) == False
+
+
+def test_can_reach_for_wb_beside_opponent_k():
+    wb_besides_opponent_k = Bishop(2, 2, True)
+    B_case_of_wb_besides_opponent_k = (5, [wb1, bb1, wb2, bb2, wb3, wk1, bk1, wb_besides_opponent_k])
+    assert bk1.can_reach(2, 2, B_case_of_wb_besides_opponent_k) == True
+
+    # todo case for can_reach
+    # less than 1
+
+
 def test_can_move_to1():
     assert wb2.can_move_to(5, 5, B1) == False
+
+
+def test_can_move_to_for_bishop_ok():
+    wk_moved = King(4, 1, True)
+    B_moved_wk_for_ok = (5, [wb1, bb1, wb2, bb2, wb3, wk_moved, bk1])
+    assert wb2.can_move_to(5, 5, B_moved_wk_for_ok) == True
+
+
+def test_can_move_to_for_king_ok():
+    assert wk1.can_move_to(4, 5, B1) == True
+
+
+def test_can_move_to_for_king_ng():
+    assert wk1.can_move_to(3, 4, B1) == False
+
+    # ng to ok
+    bk_moved = King(2, 2, False)
+    B_wk_moved_for_ok = (5, [wb1, bb1, wb2, bb2, wb3, wk1, bk_moved])
+    assert wk1.can_move_to(3, 4, B_wk_moved_for_ok) == True
+
+    # todo case for get opponent
 
 
 def test_move_to1():
@@ -71,14 +190,64 @@ def test_move_to1():
         assert found
 
 
+def test_move_to_for_king():
+    wk1_after = King(4, 5, True)
+    Actual_B = wk1.move_to(4, 5, B1)
+    Expected_B = (5, [wb1, bb1, wb2, bb2, wb3, wk1_after, bk1])
+
+    # check if actual board has same contents as expected
+    assert Actual_B[0] == 5
+
+    for piece1 in Actual_B[
+        1]:  # we check if every piece in Actual_B is also present in Expected_B; if not, the test will fail
+        found = False
+        for piece in Expected_B[1]:
+            if piece.pos_x == piece1.pos_x and piece.pos_y == piece1.pos_y and piece.side == piece1.side and type(
+                    piece) == type(piece1):
+                found = True
+
+        assert found
+
+    for piece in Expected_B[
+        1]:  # we check if every piece in Expected_B is also present in Actual_B; if not, the test will fail
+        found = False
+        for piece1 in Actual_B[1]:
+            if piece.pos_x == piece1.pos_x and piece.pos_y == piece1.pos_y and piece.side == piece1.side and type(
+                    piece) == type(piece1):
+                found = True
+        assert found
+
+    # todo case for move_to
+    # king get opponent
+
+
 def test_is_check1():
     B2 = (5, [wb1, wk1, bk1, bb1, bb2, wb3])
     assert is_check(True, B2) == True
 
 
+def test_is_check_by_bishop_false():
+    B2 = (5, [wb1, wk1, bk1, bb1, bb2, wb3])
+    assert is_check(False, B2) == False
+    B_for_false = (5, [wb1, wk1, bk1, bb1, wb3])
+    assert is_check(True, B_for_false) == False
+
+
+def test_is_check_by_king_visualiser():
+    wk1_for_true = King(3, 3, True)
+    B_wk1_for_true = (5, [wb1, wk1_for_true, bk1, bb1, wb3])
+    assert is_check(True, B_wk1_for_true) == True
+    assert is_check(False, B_wk1_for_true) == True
+
+
 def test_is_checkmate1():
     B3 = (5, [wk1a, wb4, bk1, bb2, bb3, wb3, wb5])
     assert is_checkmate(False, B3) == True
+
+
+def test_is_checkmate_for_ng():
+    B3_fixed_for_false = (5, [wk1a, wb4, bk1, bb2, wb3, wb5])
+    assert is_checkmate(False, B3_fixed_for_false) == False
 
 
 def test_read_board1():
@@ -94,6 +263,29 @@ def test_read_board1():
         assert found
 
     for piece1 in B1[1]:  # we check if every piece in B1 is also present in B; if not, the test will fail
+        found = False
+        for piece in B[1]:
+            if piece.pos_x == piece1.pos_x and piece.pos_y == piece1.pos_y and piece.side == piece1.side and type(
+                    piece) == type(piece1):
+                found = True
+        assert found
+
+
+def test_read_board_for_B2():
+    B2 = (5, [wb1, wk1, bk1, bb1, bb2, wb3])
+
+    B = read_board("board_examp_b2.txt")
+    assert B[0] == 5
+
+    for piece in B[1]:
+        found = False
+        for piece1 in B2[1]:
+            if piece.pos_x == piece1.pos_x and piece.pos_y == piece1.pos_y and piece.side == piece1.side and type(
+                    piece) == type(piece1):
+                found = True
+        assert found
+
+    for piece1 in B2[1]:
         found = False
         for piece in B[1]:
             if piece.pos_x == piece1.pos_x and piece.pos_y == piece1.pos_y and piece.side == piece1.side and type(
